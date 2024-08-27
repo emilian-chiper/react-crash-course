1ab3b4f75c2688879eb0aa268627ec81e946c442
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-08-27 11:31:33 +0300

    Complete JSX crash course

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  const name = 'John';
+  const x = 10;
+  const y = 20;
+  const names = ['Brad', 'Mary', 'Joe', 'Sara'];
+  const loggedIn = true;
+  const styles = {
+    color: 'red',
+    fontSize: '55px',
+  };
+
+  return (
+    <>
+      <div className="text-5xl">App</div>
+      <p style={styles}>Hello {name}</p>
+      <p>
+        The sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+        {}
+      </ul>
+      {loggedIn && <h1>Hello Member</h1>}
+    </>
+  );
 };
 
 export default App;
