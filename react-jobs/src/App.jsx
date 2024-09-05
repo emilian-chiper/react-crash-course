e381cc679b6c9b2e16508526b4db4305abf77dd7
emilian-chiper <emilian.chiper.developer@gmail.com>
2024-09-05 08:48:45 +0300

    Create delete job function and map to button

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import HomePage from './pages/HomePage';
-import JobsPage from './pages/JobsPage';
-import NotFoundPage from './pages/NotFoundPage';
-import JobPage, { jobLoader } from './pages/JobPage';
-import AddJobPage from './pages/AddJobPage';
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+  // Add New Job
   const addJob = async (newJob) => {
-    const res = await fetch('/api/jobs', {
-      method: 'POST',
+    const res = await fetch("/api/jobs", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newJob),
     });
     return;
+  };
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    console.log("delete", id);
   };
 
   const router = createBrowserRouter(
@@ -30,7 +36,11 @@ const App = () => {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
--- react-jobs/src/pages/JobsPage.jsx
+++ react-jobs/src/pages/JobsPage.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import JobListings from '../components/JobListings';
+import React from "react";
+import JobListings from "../components/JobListings";
 
-const JobsPage = ({}) => {
+const JobsPage = ({ deleteJob }) => {
   return (
     <section className="bg-blue-50 px-4 py-6">
       <JobListings />
